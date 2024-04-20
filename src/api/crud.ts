c2664a7191d079aa06e616297875cc13dff46abd
aaabrham <aaabrham421@gmail.com>
2024-04-20 15:16:28 +0300

    add crud methods

---
+++ src/api/crud.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { VITE_API_URL } from "../utils/env";
+
+export const get = (url: string, params?: any): any => {
+  return axios
+    .get(`${VITE_API_URL}/${url}`, params ? { params } : {})
+    .then((res) => res.data);
+};
+
+export const create = (url: string, data: any): any => {
+  return axios.post(`${VITE_API_URL}/${url}`, data).then((res) => res.data);
+};
+
+export const update = (url: string, data: any): any => {
+  return axios.put(`${VITE_API_URL}/${url}`, data).then((res) => res.data);
+};
+
+export const remove = (url: string): any => {
+  return axios.delete(`${VITE_API_URL}/${url}`).then((res) => res.data);
+};
