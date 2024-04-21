26ea867349573584712484d20caa6e6c2b87f207
aaabrham <aaabrham421@gmail.com>
2024-04-21 23:12:17 +0300

    add users(home) page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,59 @@
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useUsers } from "../hooks/useUsers";
+import { removeUsers, usersFetchStart } from "../store/user/user.slice";
+import Card from "../components/Card";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data: users, total, isLoading, error, page, limit } = useUsers();
+
+  const [noMoreUsers, setNoMoreUsers] = useState(false); // if there is no more user data left to fetch
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(removeUsers());
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    if (page > 0 && limit * page >= total)
+      setNoMoreUsers(true); // if we have retrieved all users
+    else setNoMoreUsers(false);
+  }, [page, limit, total]);
+
+  const fetchUsers = () => {
+    dispatch(usersFetchStart({ page: page + 1, limit }));
+  };
+
+  if (isLoading && users.length === 0) {
+    return (
+      <>
+        <div className="h-full gap-x-6 gap-y-5 grid grid-cols-4 py-20">
+          {new Array(4).fill(1).map((_, i) => (
+            <div key={i}>
+              <div className="animate-pulse h-56 rounded-t-xl w-full bg-slate-200"></div>
+              <div className=" h-48 rounded-b w-full bg-slate-100">
+                <div className="px-10 py-4">
+                  <div className="w-full h-10 bg-slate-200 rounded animate-pulse"></div>
+                  <div className="h-5 w-24 bg-slate-200 mt-3 rounded-lg"></div>
+                  <div className="h-5 w-48 bg-slate-200 mt-3 rounded-lg"></div>
+                  <div className="h-5 w-48 bg-slate-200 mt-3 rounded-lg"></div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="py-20">
+      <div className="gap-x-6 gap-y-5 grid grid-cols-4">
+        {users.map((user, index) => (
+          <Card user={user} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
