3f78b7f83c4b0c9486f07bef47e51efd24bc670f
aaabrham <aaabrham421@gmail.com>
2024-04-22 17:53:38 +0300

    add success option for toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Toast({
   message,
   time = 4000,
+  severity = "error",
 }: {
   message: string;
   time?: number;
+  severity?: string;
 }) {
   const ref = useRef<HTMLDivElement>();
 
@@ -26,10 +28,18 @@ export default function Toast({
     <div
       ref={(el) => (el ? (ref.current = el) : null)}
       id="toast-default"
-      className="fixed flex bottom-4 right-4 z-[99999] items-center w-full max-w-xs p-4 rounded-lg shadow text-white bg-red-500"
+      className={`fixed flex bottom-4 right-4 z-[99999] items-center w-full max-w-xs p-4 rounded-lg shadow text-white  ${
+        severity === "error" ? "bg-red-500" : "bg-green-500"
+      }`}
       role="alert"
     >
-      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-red-800 text-red-200">
+      <div
+        className={`inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg ${
+          severity === "error"
+            ? "bg-red-800 text-red-200"
+            : "bg-green-800 text-green-200"
+        }`}
+      >
         <svg
           className="w-5 h-5"
           aria-hidden="true"
@@ -44,7 +54,9 @@ export default function Toast({
       <div className="ms-3 text-sm font-normal">{message}</div>
       <button
         type="button"
-        className="ms-auto -mx-1.5 -my-1.5 text-white hover:bg-red-800 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex items-center justify-center h-8 w-8"
+        className={`ms-auto -mx-1.5 -my-1.5 text-white rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex items-center justify-center h-8 w-8 ${
+          severity === "error" ? "hover:bg-red-800" : "hover:bg-green-800"
+        }`}
         data-dismiss-target="#toast-default"
         aria-label="Close"
         onClick={closeToast}
